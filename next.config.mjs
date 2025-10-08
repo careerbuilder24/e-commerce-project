51b80037d5d8ddfd805aeb201824e28583f88120
careerbuilder24 <careerbuilder.com.bd@gmail.com>
2025-10-08 16:12:26 +0600

    fic home redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -6,9 +8,40 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
+    async redirects() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+        permanent: false,
+      },
+    ];
+  },
+
+
   images: {
     unoptimized: true,
+    domains: [
+      "i.ibb.co",
+      "i.ibb.co.com",
+      "lh3.googleusercontent.com",
+      "ibb.co",
+      "img.freepik.com",
+      "i.postimg.cc",
+      "postimg.cc"
+    ],
   },
-}
 
-export default nextConfig
+  reactStrictMode: false,
+
+  devIndicators: {
+    buildActivity: false,
+  },
+
+
+};
+
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})(nextConfig);
